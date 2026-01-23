e56126c9b8acdc450cb540e45fadd418dacb5fd3
plcongg270906 <phanlapcong182@gmail.com>
2026-01-23 21:44:49 +0700

    Remove

--- Bai1_Hello/src/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-
-public class HelloWorld {
-
-	public static void main(String[] args) {
-		System.out.print("Chào Java");// TODO Auto-generated method stub
-		System.out.print("Chào Java1");
-	}
-
-}
