3beb349f82700955cd208c4ca28822f5532b4efe
plcongg270906 <phanlapcong182@gmail.com>
2026-01-23 21:42:26 +0700

    Upload HelloWorld.java

---
+++ Bai1_Hello/src/HelloWorld.java
@@ -0,0 +1,9 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.print("Chào Java");// TODO Auto-generated method stub
+		System.out.print("Chào Java1");
+	}
+
+}
