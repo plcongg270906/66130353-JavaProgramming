0517d0d81bcecd435ee256a55f4980a15f3c4104
plcongg270906 <phanlapcong182@gmail.com>
2026-01-25 21:41:23 +0700

    Upload btap 1

---
+++ Bai1_Hello/src/Btap1.java
@@ -0,0 +1,10 @@
+
+public class Btap1 {
+
+	public static void main(String[] args) {
+		System.out.println("Tên: Phan Lập Công");
+        System.out.println("Lớp: 66.CNTT-1");
+        System.out.println("MSSV: 66130353");
+	}
+
+}
