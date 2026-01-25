26ad2ab4c6d635abc367cb242ecf39bf476e370d
plcongg270906 <phanlapcong182@gmail.com>
2026-01-25 21:58:03 +0700

    Upload toan bo btap con lai cua bai 1

---
+++ Bai1_Hello/src/Btap3.java
@@ -0,0 +1,12 @@
+
+public class Btap3 {
+
+	public static void main(String[] args) {
+		System.out.println("5 + 3 = " + (5+3));
+		System.out.println("5 - 3 = " + (5-3));
+		System.out.println("5 * 3 = " + (5*3));
+		System.out.println("5 / 3 = " + (5/3));
+		System.out.println("5 % 3 = " + (5%3));
+	}
+
+}
