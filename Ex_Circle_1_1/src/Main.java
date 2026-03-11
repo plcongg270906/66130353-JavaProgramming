483115c9c51016edf101ad93f266227f39015fbb
plcongg270906 <phanlapcong182@gmail.com>
2026-03-11 20:05:29 +0700

    Upload bai 1 Exercises on Classes

---
+++ Ex_Circle_1_1/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Circle c1 = new Circle();
+        System.out.println("The circle has radius of " + c1.getRadius() + " and area of " + c1.getArea());
+        
+        Circle c2 = new Circle(3.0);
+        System.out.println("The circle has radius of " + c2.getRadius() + " and area of " + c2.getArea());
+        
+        Circle c3 = new Circle(5.5, "blue");
+        System.out.println(c3.toString());
+
+	}
+
+}
