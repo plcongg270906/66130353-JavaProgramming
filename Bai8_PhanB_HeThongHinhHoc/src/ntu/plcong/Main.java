a857fa41f0d2a1d3b01be1721cc3c10d7fd658b1
plcongg270906 <phanlapcong182@gmail.com>
2026-03-25 23:25:49 +0700

    Update main
    thêm hình 4

--- Bai8_PhanB_HeThongHinhHoc/src/ntu/plcong/Main.java
+++ Bai8_PhanB_HeThongHinhHoc/src/ntu/plcong/Main.java
@@ -6,12 +6,15 @@ public class Main {
 		HinhChuNhat h1 = new HinhChuNhat("Hinh chu nhat","Do", 4, 5);
 		HinhTron h2 = new HinhTron("Hinh tron","Xanh", 3);
 		HinhTamGiac h3 = new HinhTamGiac("Hinh tam giac","Vang", 3, 4, 5);
+		HinhTamGiac h4 = new HinhTamGiac("Hinh tam giac","Vang", 10, 2, 2);
 		
 		h1.hienThiThongTin();
 		System.out.println("=======================");
 		h2.hienThiThongTin();
 		System.out.println("=======================");
 		h3.hienThiThongTin();
+		System.out.println("=======================");
+		h4.hienThiThongTin();
 		
 	}
 
