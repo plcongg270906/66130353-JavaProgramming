14d56f541ff09d664c037e6dc2183080538001f2
plcongg270906 <phanlapcong182@gmail.com>
2026-05-13 23:45:22 +0700

    commit javafx01 lần 1

---
+++ HelloJavaFX001/src/plcongg/apps/HelloFX01.java
@@ -0,0 +1,22 @@
+package plcongg.apps;
+
+import javafx.application.Application;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class HelloFX01 extends Application{
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Button btn = new Button();
+		
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	
+
+}
