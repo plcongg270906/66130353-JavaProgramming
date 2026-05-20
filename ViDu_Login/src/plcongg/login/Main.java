716c809010f6f690ac6e0e5af1a1506105b577cf
plcongg270906 <phanlapcong182@gmail.com>
2026-05-20 22:25:54 +0700

    commit ví dụ login lần 2

--- ViDu_Login/src/plcongg/login/Main.java
+++ ViDu_Login/src/plcongg/login/Main.java
@@ -1,6 +1,8 @@
 package plcongg.login;
 	
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -30,7 +32,30 @@ public class Main extends Application {
 		GridPane gridpane = new GridPane();
 		//setting size for the pane
 		gridpane.setMinSize(400, 200);
+		//setting the padding
+		gridpane.setPadding(new Insets(10, 10, 10, 10));
 		
+		//setting the vertical and horizontal gaps between the columns
+		gridpane.setVgap(5);
+		gridpane.setHgap(5);
+		//setting the grid alignment
+		gridpane.setAlignment(Pos.CENTER);
+		//Arranging all the nodes in the grid
+		gridpane.add(text1, 0, 0);
+		gridpane.add(textField1, 1, 0);
+		gridpane.add(text2, 0, 1);
+		gridpane.add(textField2, 1, 1);
+		gridpane.add(btn1, 0, 2);
+		gridpane.add(btn2, 1, 2);
+		
+		//create a scene object
+		Scene sc = new Scene(gridpane);
+		//setting title to the stage
+		primaryStage.setTitle("Example Login");
+		//adding scene to the stage
+		primaryStage.setScene(sc);
+		//displaying the contents of the stage;
+		primaryStage.show();
 	}
 	
 	public static void main(String[] args) {
